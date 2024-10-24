cba8be643b4a0f92df6a54d4e06babe289a54fd3
Mark Corrales <corrales.mark@epa.gov>
2024-10-24 15:36:31 -0400

    testdata in root

--- R/republish.R
+++ R/republish.R
@@ -1,7 +1,8 @@
 
 republish <- function() {
 
-  donow <- askYesNo("Update metadata first, if relevant!\nSee source package data-raw folder for details.\nPUBLISH NOW?")
+  donow <- askYesNo(
+    "To publish new files in pkgdown assets but not data\nin pins, do init_site() not this.\nUpdate metadata first, if relevant!\nSee source package data-raw folder for details.\nPUBLISH NOW?")
   if (is.na(donow)) {cat("halted\n")} else {
     if (donow) {
       cat("\n\nDoing pkgdown::build_site() now ... \n\n")
